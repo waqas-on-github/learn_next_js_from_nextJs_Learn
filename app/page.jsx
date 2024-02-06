d2aab778bdeba77033144f77eb68a55e8d186062
waqas-on-github <waqasvu892@gmail.com>
2024-02-06 23:53:35 +0500

    resetted project

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3xl font-bold">NEXT JS 14 STARTER KIT</h1>
+      </div>
+    </section>
+  );
+}
